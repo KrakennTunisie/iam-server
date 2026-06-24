86e698aaa41bbbfc8c8f06a0553c26097c4755f2
OumaimaKouka <oumayma.chelly@kouka.io>
2026-06-24 17:46:07 +0200

    feat: implementing classs user and role

--- src/main/java/com/krakennTunisie/IAM_server/domain/model/Role.java
+++ src/main/java/com/krakennTunisie/IAM_server/domain/model/Role.java
@@ -1,4 +1,10 @@
 package com.krakennTunisie.IAM_server.domain.model;
 
 public class Role {
+    private String id;
+    private String name;
+    private String description;
+
+    private boolean clientRole;
+
 }
--- src/main/java/com/krakennTunisie/IAM_server/domain/model/User.java
+++ src/main/java/com/krakennTunisie/IAM_server/domain/model/User.java
@@ -1,4 +1,23 @@
 package com.krakennTunisie.IAM_server.domain.model;
 
+import java.util.List;
+
 public class User {
+
+    private String keycloakId;
+    private String userName;
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    private boolean enabled;
+    private boolean emailVerified;
+
+    private String temporaryPassword;
+
+
+    private List<Role> roles;
+
+
+
 }
