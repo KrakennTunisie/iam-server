e2373afe5cea1d0f82ad7ffe538f23a004ea4dde
OumaimaKouka <oumayma.chelly@kouka.io>
2026-08-06 15:52:53 +0200

    fix: fixing class test

--- src/test/java/com/krakennTunisie/IAM_server/IamServerApplicationTests.java
+++ src/test/java/com/krakennTunisie/IAM_server/IamServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.krakennTunisie.IAM_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = "spring.cloud.config.enabled=false")
+@ActiveProfiles("test")
 class IamServerApplicationTests {
 
 	@Test
