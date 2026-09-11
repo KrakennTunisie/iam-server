d1ecfcdf196233b18059a1ce0edf0dd1075c8aa6
Wassef-Ammar <wassef.ammar@kouka.io>
2026-09-11 15:52:05 +0200

    fix: fix tests issues

--- src/test/java/com/krakennTunisie/IAM_server/IamServerApplicationTests.java
+++ src/test/java/com/krakennTunisie/IAM_server/IamServerApplicationTests.java
@@ -2,11 +2,17 @@ package com.krakennTunisie.IAM_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(properties = "spring.cloud.config.enabled=false")
 @ActiveProfiles("test")
 class IamServerApplicationTests {
+
+
+	@MockitoBean
+	private JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
